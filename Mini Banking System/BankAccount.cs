b2eff6591656979d6400eef068643ed9a941a524
Tony Wang <tony94042@gmail.com>
2026-03-09 16:08:41 +1100

    Added base class

---
+++ Mini Banking System/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Banking_System
+{
+    public abstract class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public string HolderName { get; set; }
+        public decimal Balance { get; protected set; }
+        public abstract decimal WithDraw(decimal amount);
+
+        public decimal Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+            Balance += amount;
+            return Balance;
+        }
+
+        public virtual void DisplayAccountInfo()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}");
+            Console.WriteLine($"Holder Name: {HolderName}");
+            Console.WriteLine($"Balance: {Balance:C}");
+        }
+    }
+
+};
